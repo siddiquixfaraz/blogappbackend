81a8db4e45161c54c70bd6471f3edf024e5763f5
siddiquixfaraz <109731664+siddiquixfaraz@users.noreply.github.com>
2023-10-02 10:50:00 +0530

    schema updated

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -3,7 +3,13 @@ import mongoose from "mongoose";
 const blogPostSchema = new mongoose.Schema({
     title: String,
     description: String,
-});
+    photo: {
+        type: String,
+        required: false,
+    },
+
+}
+, { timestamps: true });
 
 
 export const BlogPost = mongoose.model("BlogPost",blogPostSchema);
