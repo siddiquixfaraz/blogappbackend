e750555dcd2d6746426fda1da07b29fe6bd00b2d
siddiquixfaraz <109731664+siddiquixfaraz@users.noreply.github.com>
2023-10-02 20:42:41 +0530

    image upload fixed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { connectDb } from "./data/database.js";
 import cors from "cors";
 import { config } from "dotenv";
 import router from "./routes/blogRoutes.js";
+import multer from "multer"
 
 const app = express();
 
@@ -18,6 +19,21 @@ app.use(router)
 
 
 connectDb();
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null,"images")
+    },
+    filename: (req,file,cb)=>{
+        cb(null,req.body.name)
+    }
+})
+
+const upload = multer({storage: storage})
+
+app.post("/upload", upload.single("file"),(req,res)=>{
+    res.status(200).json({message:"file uploaded"})
+})
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on PORT ${process.env.PORT}`)
